e05856bf1ea443863bdfac0be402def8ba21a2b4
Vanya <ranzar2000@gmail.com>
2026-04-11 18:54:00 +0300

    PART-1 finish 1.3-1.5 exercises

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,5 @@
-const Header = ({ title }) => {
-  return <h1>{title}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
 };
 
 const Part = ({ name, count }) => {
@@ -10,38 +10,45 @@ const Part = ({ name, count }) => {
   );
 };
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={part1.name} count={part1.count} />
-      <Part name={part2.name} count={part2.count} />
-      <Part name={part3.name} count={part3.count} />
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} count={part.exercises} />
+      ))}
     </div>
   );
 };
 
-const Total = ({ sum }) => {
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => acc + part.exercises, 0);
   return <p>Number of exercises {sum}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header title={course} />
-      <Content
-        part1={{ name: part1, count: exercises1 }}
-        part2={{ name: part2, count: exercises2 }}
-        part3={{ name: part3, count: exercises3 }}
-      />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
