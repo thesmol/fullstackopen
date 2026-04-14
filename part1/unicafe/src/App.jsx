e4472e3609ab6d9df59740b90ceb6ca53e1cad6f
Vanya <ranzar2000@gmail.com>
2026-04-14 17:21:44 +0300

    PART-1 Init unicafe and finish 1.6 exercise

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const ButtonsGroup = ({ buttons }) => {
+  return (
+    <div>
+      {buttons.map((button) => (
+        <Button key={button.text} onClick={button.onClick} text={button.text} />
+      ))}
+    </div>
+  );
+};
+
+const Stat = ({ text, value }) => {
+  return (
+    <div>
+      <span>
+        {text}: {value}
+      </span>
+    </div>
+  );
+};
+
+const Statistic = ({ statistic }) => {
+  return (
+    <div>
+      {statistic.map((stat) => (
+        <Stat key={stat.text} text={stat.text} value={stat.value} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Header text="Give feedback" />
+      <ButtonsGroup
+        buttons={[
+          { text: "Good", onClick: handleGoodClick },
+          { text: "Neutral", onClick: handleNeutralClick },
+          { text: "Bad", onClick: handleBadClick },
+        ]}
+      />
+      <Header text="Statistics" />
+      <Statistic
+        statistic={[
+          { text: "Good", value: good },
+          { text: "Neutral", value: neutral },
+          { text: "Bad", value: bad },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default App;
