43f32f35611c7467164ac6bce6be5c96ae40742d
Vanya <ranzar2000@gmail.com>
2026-04-15 18:56:35 +0300

    PART-2 finish exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Course/Course.jsx
+++ part2/courseinfo/src/components/Course/Course.jsx
@@ -1,11 +1,13 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
