f6da2109d5a72531fe4d7cd429f75d9ab383cc5a
Vanya <ranzar2000@gmail.com>
2026-04-16 19:26:16 +0300

    PART-2 finish exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,53 @@
 import { useState } from "react";
 
-const Person = ({ name }) => {
-  return <p>{name}</p>;
+const Person = ({ person }) => {
+  return (
+    <p>
+      {person.name}, {person.phone}
+    </p>
+  );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567" },
+  ]);
+  const [newPerson, setNewPerson] = useState({
+    name: "",
+    phone: "",
+  });
 
   const changeName = (event) => {
-    setNewName(event.target.value);
+    setNewPerson({ ...newPerson, name: event.target.value });
   };
 
-  const checkUserExists = (name) => {
-    return persons.some((person) => person.name === name);
+  const changePhone = (event) => {
+    setNewPerson({ ...newPerson, phone: event.target.value });
+  };
+
+  const checkUserExists = () => {
+    return persons.some(
+      (p) => p.name === newPerson.name && p.phone === newPerson.phone,
+    );
   };
 
   const addNewPerson = (event) => {
     event.preventDefault();
 
-    if (checkUserExists(newName)) {
-      alert(`${newName} is already added to Phonebook`);
+    if (newPerson.name === "" || newPerson.phone === "") {
+      alert("Please fill in both name and phone fields.");
       return;
     }
 
-    const newPerson = {
-      name: newName,
-    };
+    if (checkUserExists()) {
+      alert(
+        `${newPerson.name} with phone ${newPerson.phone} is already added to phonebook`,
+      );
+      return;
+    }
+
     setPersons(persons.concat(newPerson));
-    setNewName("");
+    setNewPerson({ name: "", phone: "" });
   };
 
   return (
@@ -36,8 +55,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name:{" "}
-          <input value={newName} onChange={changeName} placeholder="John Doe" />
+          name:
+          <input
+            value={newPerson.name}
+            onChange={changeName}
+            placeholder="John Doe"
+          />
+        </div>
+        <div>
+          phone:
+          <input
+            value={newPerson.phone}
+            onChange={changePhone}
+            placeholder="+358..."
+          />
         </div>
         <div>
           <button type="submit" onClick={addNewPerson}>
@@ -46,8 +77,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <Person key={person.name + index} name={person.name} />
+      {persons.map((person) => (
+        <Person key={person.name + person.phone} person={person} />
       ))}
     </div>
   );
