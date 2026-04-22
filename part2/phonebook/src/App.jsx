788993c276d38a87a620910db0ea4a3fbd9dac97
Vanya <ranzar2000@gmail.com>
2026-04-22 17:58:43 +0300

    PART-2 finish exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PersonsList from "./components/PersonsList";
 import Filter from "./components/Filter";
 import NewPersonForm from "./components/NewPersonForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456", id: 1 },
-    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newPerson, setNewPerson] = useState({
     name: "",
@@ -18,6 +14,13 @@ const App = () => {
   });
 
   const [filterName, setFilterName] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const changeFilter = (event) => {
     setFilterName(event.target.value);
