d6b003d9b4ae22d2eb21a39d86e1558114656a78
Vanya <ranzar2000@gmail.com>
2026-05-02 23:46:18 +0300

    PART-3 Finish the 7th exercise

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -93,6 +97,12 @@ app.post("/api/persons", (request, response) => {
 
   response.json(person);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
