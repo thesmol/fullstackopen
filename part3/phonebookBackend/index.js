f77c816d34dea17848e3569faed1cbb82cd66fbc
Vanya <ranzar2000@gmail.com>
2026-05-09 21:21:30 +0300

    PART-3 setup proxy and delete cors

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -10,9 +10,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-const cors = require("cors");
-app.use(cors());
-
 app.use(express.static("dist"));
 
 let persons = [
