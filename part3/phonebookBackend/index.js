766e8673429af1aa7c0ce24b64fa5039c85d65d6
Vanya <ranzar2000@gmail.com>
2026-05-09 20:25:38 +0300

    PART-3 install cors

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -10,6 +10,9 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
+const cors = require("cors");
+app.use(cors());
+
 let persons = [
   {
     id: "1",
@@ -108,7 +111,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
