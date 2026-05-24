40f776906af1021a3ad2982def8c62b9d5def0fc
Vanya <ranzar2000@gmail.com>
2026-05-24 22:00:40 +0300

    PART-4 start refactor, move Blog to the separate module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,25 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (_document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, { family: 4 });
