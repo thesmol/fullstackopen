d4496ca876303494fefdbe260bd8ad12c690b4b4
Vanya <ranzar2000@gmail.com>
2026-05-24 22:12:35 +0300

    PART-4 Move env config to the separate module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const blogRoutes = require("./controllers/blogs");
+const config = require("./utils/config");
 
 const app = express();
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { family: 4 });
 
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
