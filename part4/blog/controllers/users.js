46130356c45bceb2fa2ba331b2104ca4e8e2b4c7
Vanya <ranzar2000@gmail.com>
2026-06-11 13:23:42 +0300

    PART-4 also fix users controller

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -31,13 +31,13 @@ usersRouter.get("/:id", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
-  const errors = ["Validation failed"];
+  const errors = [];
   if (password && password.length < 3)
     errors.push("Password must contains at least 3 characters");
   if (username && username.length < 3)
     errors.push("Username must contains at least 3 characters");
 
-  if (errors.length > 1)
+  if (errors.length > 0)
     return response.status(400).json({ error: errors.join(", ") });
 
   const passwordHash = password ? await bcrypt.hash(password, 10) : undefined;
@@ -63,8 +63,14 @@ usersRouter.put("/:id", async (request, response) => {
   foundUser.passwordHash = passwordHash;
 
   const savedUser = await foundUser.save();
+  const populated = await savedUser.populate("blogs", {
+    author: 1,
+    title: 1,
+    likes: 1,
+    url: 1,
+  });
 
-  response.json(savedUser);
+  response.json(populated);
 });
 
 usersRouter.delete("/:id", async (request, response) => {
