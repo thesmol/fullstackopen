a5b338193f501f1777682e28d40e735e30883f7d
Vanya <ranzar2000@gmail.com>
2026-06-13 21:16:02 +0300

    PART-4 Finish the 4.21 exercise

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -67,7 +67,22 @@ blogsRouter.put("/:id", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+  const blog = await Blog.findById(request.params.id);
+
+  if (!blog) {
+    return response.status(404).end();
+  }
+
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(403).json({ error: "forbidden" });
+  }
+
+  await Blog.deleteOne({ _id: request.params.id });
   response.status(204).end();
 });
 
